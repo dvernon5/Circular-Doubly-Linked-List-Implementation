dd2726cbb27a902f28c256ddaee0258a9984d6bd
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-16 23:54:20 -0800

    Create LinkedListManagement.cpp Define Circular Doubly Linked List functionality to control the system.

---
+++ LinkedListManagement.cpp
@@ -0,0 +1,138 @@
+#include "LinkedListManagement.h"
+#include <iostream>
+
+namespace InputValidation {
+  void ClearInputBuffer() {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
+
+  int GetValidatedInput(int user_input) {
+    while (!(std::cin >> user_input)) {
+      std::cout << "Invalid type. Please enter a number.\n";
+      ClearInputBuffer();
+    }
+    ClearInputBuffer();
+    return user_input;
+  }
+} using namespace InputValidation;
+
+LinkedListManagement::LinkedListManagement() {
+  root_node = nullptr;
+}
+void LinkedListManagement::InsertRootNode() {
+  root_node             = new Node;
+  list_info.AllocationWarningMessage(root_node);
+  root_node->prev_node  = root_node;
+  root_node->data       = node.data;
+  root_node->next_node  = root_node;
+  std::cout << "Your data " << node.data << " was successfully added.\n";
+}
+
+void LinkedListManagement::InsertAtFirstNode(NodePositionInfo& node) {
+  struct Node* new_node = new Node;
+  list_info.AllocationWarningMessage(new_node);
+  new_node->prev_node             = root_node->prev_node;
+  new_node->data                  = node.data;
+  new_node->next_node             = root_node;
+  new_node->prev_node->next_node  = new_node;
+  root_node->prev_node            = new_node;
+  root_node                       = new_node;
+  std::cout << "Your data " << node.data << " was successfully added.\n";
+}
+
+void LinkedListManagement::InsertNodeAtPosition(NodePositionInfo& node, int total_nodes) {
+  struct Node* ptr_node = root_node;
+  for (size_t position  = 0; position < node.position - 1 && ptr_node->next_node != root_node; ++position) {
+    ptr_node  = ptr_node->next_node;
+  }
+  struct Node* new_node   = new Node;
+  list_info.AllocationWarningMessage(new_node);
+  if (node.position == total_nodes + 1) {
+    new_node->prev_node   = ptr_node;
+    new_node->data        = node.data;
+    new_node->next_node   = root_node;
+    ptr_node->next_node   = new_node;
+    root_node->prev_node  = new_node;
+  } else {
+    new_node->prev_node            = ptr_node->prev_node;
+    new_node->data                 = node.data;
+    new_node->next_node            = ptr_node;
+    ptr_node->prev_node->next_node = new_node;
+    ptr_node->prev_node            = new_node;
+  }
+}
+
+void LinkedListManagement::InsertNode(NodePositionInfo& node) {
+  const int BEGINNING_POSITION = 1;
+  int total_nodes = list_info.GetNumberOfNodes(root_node);
+  if (root_node == nullptr) {
+    InsertRootNode();
+  } else if (node.position == BEGINNING_POSITION) {
+    InsertAtFirstNode(node);
+  } else if (node.position > BEGINNING_POSITION && node.position <= total_nodes + 1) {
+    InsertNodeAtPosition(node, total_nodes);
+  } else {
+    std::cout << "Invalid position! Please enter a number between " << BEGINNING_POSITION
+              << " and " << total_nodes + 1 << "\n";
+  }
+}
+
+// TODO: Build Delete routines.
+
+void LinkedListManagement::PrintList(struct Node* ptr_node) {
+  if (list_info.IsEmpty(ptr_node)) {
+    std::cout << "List is empty.";
+    return;
+  }
+  do {
+    std::cout << ptr_node->data << "<-->";
+    ptr_node = ptr_node->next_node;
+  } while (ptr_node->next_node != root_node);
+  std::cout << ptr_node->data << "<-->" << root_node->data << "\n";
+}
+
+void LinkedListManagement::PrintMenu() {
+  std::cout << "\nWelcome to Linked List Management System.\n";
+  std::cout << "1. Insert\n";
+  std::cout << "2. Delete\n";
+  std::cout << "3. List Nodes\n";
+  std::cout << "4. Exit\n";
+  std::cout << "Selection >> ";
+}
+
+void LinkedListManagement::LaunchLinkedListManagement() {
+  int selection{0};
+  do {
+    PrintMenu();
+    selection = GetValidatedInput(selection);
+    switch (selection) {
+      case 1: {
+        std::cout << "\nADD NODE: \n---------\n";
+        std::cout << "Enter a number: ";
+        node.data = GetValidatedInput(node.data);
+        if (root_node != nullptr) {
+          int total_nodes = list_info.GetNumberOfNodes(root_node);
+          std::cout << "Enter a position between 1 and " << total_nodes + 1 << " to insert node: ";
+          node.position   = GetValidatedInput(node.position);
+        }
+        InsertNode(node);
+        break;
+      }
+      case 2: {
+       // TODO: Set up delete functionality. 
+        break;
+      }
+      case 3:
+        std::cout << "\nLIST ALL NODES: \n----------------\n";
+        PrintList(root_node);
+        break;
+      case 4:
+        std::cout << "\nThank you... Goodbye!\n";
+        break;
+      default:
+        std::cout << "Invalid selection. Try again\n";
+        break;
+    }
+  } while (selection != 4);
+}
