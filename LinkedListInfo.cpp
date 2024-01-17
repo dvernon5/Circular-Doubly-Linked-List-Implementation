c2e1281f0898a93d2ab3507cadc10ab258376242
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-16 23:42:24 -0800

    Create LinkedListInfo.cpp Define the function declare by LinkedListInfo.h.
    Define the functionalities of the current Linked List. For example, getting the number of nodes in the list. Or checking if a list is empty. Make sure that node allocation is validated before inserting it into the Linked List.

---
+++ LinkedListInfo.cpp
@@ -0,0 +1,38 @@
+#include "LinkedListInfo.h"
+#include <iostream>
+
+bool LinkedListInfo::IsAllocationError(struct Node* ptr_node) {
+  if (ptr_node == nullptr) {
+    return true;
+  }
+  
+  return false;
+}
+
+void LinkedListInfo::AllocationWarningMessage(struct Node* ptr_node) {
+  if (IsAllocationError(ptr_node)) {
+    std::cerr << "Allocation error\n";
+  }
+}
+
+bool LinkedListInfo::IsEmpty(struct Node* ptr_node) {
+  if (ptr_node == nullptr) {
+    return true;
+  }
+  
+  return false;
+}
+
+int LinkedListInfo::GetNumberOfNodes(struct Node* ptr_node) {
+  if (ptr_node == nullptr) {
+    return 0;
+  }
+  unsigned counter{0};
+  struct Node* current_node = ptr_node;
+  do {
+    ++counter;
+    current_node = current_node->next_node;
+  } while (current_node != ptr_node);
+  
+  return counter;
+}
