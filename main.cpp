171aa6189ff6565ed9289ce9eba3ec5d76cd473d
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-16 23:57:27 -0800

    Update main.cpp main has access to launch the Circular Doubly Linked List Application.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include "LinkedListManagement.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, const char* argv[]) {
+  LinkedListManagement linked_list;
+  linked_list.LaunchLinkedListManagement();
+  
   EXIT_SUCCESS;
 }
