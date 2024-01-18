7b95b1c50b21af9415c6f87b6fbdc4c298d7315e
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-18 00:17:32 -0800

    Update DeleteNodeManager.cpp Made some styling changes

--- DeleteNodeManager.cpp
+++ DeleteNodeManager.cpp
@@ -1,11 +1,17 @@
-struct Node* DeleteNodeManager::DeleteFirstNode(struct Node* current_node, NodePositionInfo& node, int total_nodes) {
+#include "DeleteNodeManager.h"
+#include <iostream>
+
+struct Node* DeleteNodeManager::DeleteFirstNode(struct Node* current_node,
+                                                NodePositionInfo& node,
+                                                int total_nodes)
+{
   const int A_SINGLE_NODE = 1;
-  /* Case: There is only root node just delete it. */
+  // Case: Root node only. Just delete it.
   if (total_nodes == A_SINGLE_NODE) {
     delete current_node;
     current_node = nullptr;
   } else {
-    /* Case: There is more than one node. Must move root node to next node and manipulate links. */
+    // Case: There is more than one node. Must move root node to next node and manipulate links.
     struct Node* temp_ptr          = current_node;
     current_node                   = current_node->next_node;
     current_node->prev_node        = temp_ptr->prev_node;
@@ -20,7 +26,7 @@ struct Node* DeleteNodeManager::DeleteFirstNode(struct Node* current_node, NodeP
 }
 
 void DeleteNodeManager::DeleteBetweenNodes(struct Node* current_node, NodePositionInfo& node) {
-  /* Case: Delete a Node at specific position. */
+  // Case: Delete a Node at specific position.
   for (int current_position = 0; current_position < node.position - 1; ++current_position) {
     current_node = current_node->next_node;
   }
