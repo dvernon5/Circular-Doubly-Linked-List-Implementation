327a3422e7e0f2d6c12062956efff49ca124d584
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-17 22:29:31 -0800

    Create InsertNodeManager.h Declare routines to insert a node at a given position
    Declare routines to insert a new node either at the first node last node or anywhere between the first and last node.

---
+++ InsertNodeManager.h
@@ -0,0 +1,19 @@
+#ifndef InsertNodeManager_h
+#define InsertNodeManager_h
+#include "Node.h"
+#include "LinkedListInfo.h"
+#include "NodePositionInfo.h"
+
+class InsertNodeManager {
+  public:
+    InsertNodeManager                 () = default;
+    struct Node* InsertRootNode       (struct Node* ptr_node, NodePositionInfo& node);
+    struct Node* InsertAtFirstNode    (struct Node* ptr_node, NodePositionInfo& node);
+    struct Node* InsertNodeAtPosition (struct Node* ptr_node, NodePositionInfo& node);
+    struct Node* InsertAtBackNode     (struct Node* ptr_node, NodePositionInfo& node);
+    struct Node* InsertNode           (struct Node* ptr_node, NodePositionInfo& node);
+  
+  private:
+    LinkedListInfo list_info;
+};
+#endif /* InsertNodeManager_h */
