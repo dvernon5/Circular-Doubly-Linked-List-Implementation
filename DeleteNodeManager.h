412c4b1ff0d38c360535fa2a3dc441c2011d854d
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-17 22:55:49 -0800

    Create DeleteNodeManager.h Declare routines to delete a node at a given position
    Declare routines to delete a new node either at the first node last node or anywhere between the first and last node.

---
+++ DeleteNodeManager.h
@@ -0,0 +1,18 @@
+#ifndef DeleteNodeManager_h
+#define DeleteNodeManager_h
+#include "Node.h"
+#include "LinkedListInfo.h"
+#include "NodePositionInfo.h"
+
+class DeleteNodeManager {
+  public:
+    DeleteNodeManager            () = default;
+    struct Node* DeleteFirstNode (struct Node* current_node, NodePositionInfo& node, int total_nodes);
+    void DeleteNodeAtPosition    (struct Node* current_node, NodePositionInfo& node);
+    void DeleteBackNode          (struct Node* current_node, NodePositionInfo& node);
+    struct Node* DeleteNode      (struct Node* current_node, NodePositionInfo& node);
+  
+  private:
+    LinkedListInfo list_info;
+};
+#endif /* DeleteNodeManager_h */
