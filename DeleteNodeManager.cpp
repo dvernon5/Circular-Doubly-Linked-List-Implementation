05e30450f8b10b3bcf1474fdee386575c8e780cd
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-17 22:59:15 -0800

    Create DeleteNodeManager.cpp Created a rountine to Delete first node
    Created a routine named DeleteFirstNode(). Delete the first node in the list. If the list is not empty.

---
+++ DeleteNodeManager.cpp
@@ -0,0 +1,20 @@
+struct Node* DeleteNodeManager::DeleteFirstNode(struct Node* current_node, NodePositionInfo& node, int total_nodes) {
+  const int A_SINGLE_NODE = 1;
+  /* Case: There is only root node just delete it. */
+  if (total_nodes == A_SINGLE_NODE) {
+    delete current_node;
+    current_node = nullptr;
+  } else {
+    /* Case: There is more than one node. Must move root node to next node and manipulate links. */
+    struct Node* temp_ptr          = current_node;
+    current_node                   = current_node->next_node;
+    current_node->prev_node        = temp_ptr->prev_node;
+    temp_ptr->prev_node->next_node = current_node;
+    node.data                      = temp_ptr->data;
+    delete temp_ptr;
+    temp_ptr                       = nullptr;
+  }
+  std::cout << "Your data " << node.data << " was successfully deleted.\n";
+  
+  return current_node;
+}
