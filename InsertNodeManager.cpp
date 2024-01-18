e65338d2ad41b18f4c87add83dabb7692803bf0b
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-17 22:32:30 -0800

    Update InsertNodeManager.cpp Define a routine to insert a node at a position between the first and last node.

--- InsertNodeManager.cpp
+++ InsertNodeManager.cpp
@@ -29,3 +29,25 @@ struct Node* InsertNodeManager::InsertAtFirstNode(struct Node* ptr_node, NodePos
   
   return ptr_node;
 }
+
+struct Node* InsertNodeManager::InsertNodeAtPosition(struct Node* ptr_node, NodePositionInfo& node) {
+  const int POSITION_DECREMENT = 1;
+  struct Node* current_node    = ptr_node;
+  for (size_t current_position = 0;
+        current_position < node.position - POSITION_DECREMENT && current_node->next_node != ptr_node;
+       ++current_position)
+  {
+    current_node = current_node->next_node;
+  }
+  struct Node* new_node              = new Node;
+  list_info.AllocationWarningMessage(new_node);
+  new_node->prev_node                = current_node->prev_node;
+  new_node->data                     = node.data;
+  new_node->next_node                = current_node;
+  current_node->prev_node->next_node = new_node;
+  current_node->prev_node            = new_node;
+  std::cout << "Your data " << node.data  << " was successfully added at "
+            << "Position "  << node.position << ".\n";
+  
+  return ptr_node;
+}
