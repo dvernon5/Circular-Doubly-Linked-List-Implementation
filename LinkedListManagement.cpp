7a381b4055a3438886932743b9a0f973aada1670
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-18 00:00:19 -0800

    Update LinkedListManagement.cpp Updated LaunchLInkedListManagement() routine
    In LaunchLinkedListManagement() routine. I updated case 2 to implement the request manager class to inform the user to delete a node from the linked list and made the code a lot sleeker.

--- LinkedListManagement.cpp
+++ LinkedListManagement.cpp
@@ -77,17 +77,8 @@ void LinkedListManagement::LaunchLinkedListManagement() {
         break;
       }
       case 2: {
-        std::cout << "\nDELETE NODE: \n------------\n";
-        int total_nodes = list_info.GetNumberOfNodes(root_node);
-        if (total_nodes == 1) {
-          // Case: Only one node available. Prompt user to enter 1.
-          std::cout << "Please enter the number 1: ";
-        } else {
-          // Case: Multiple nodes available. Prompt user to enter a number between 1 and total nodes.
-          std::cout << "Enter a position between 1 and " << total_nodes << " to delete node: ";
-        }
-        node.position = GetValidatedInput(node.position);
-        DeleteNode(node);
+        node = request.DeleteRequest(root_node);
+        DeleteNode();
         break;
       }
       case 3:
