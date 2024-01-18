2ab854491ae69e4e43a1877fd2f2112e399b2927
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-17 23:22:52 -0800

    Update LinkedListManagement.h Included the necessary header files in order to access them throughout my program.

--- LinkedListManagement.h
+++ LinkedListManagement.h
@@ -1,8 +1,8 @@
 #ifndef LinkedListManagement_h
 #define LinkedListManagement_h
-#include "Node.h"
-#include "LinkedListInfo.h"
-#include "NodePositionInfo.h"
+#include "InsertNodeManager.h"
+#include "DeleteNodeManager.h"
+#include "RequestManager.h"
 
 class LinkedListManagement {
   public:
@@ -13,9 +13,13 @@ class LinkedListManagement {
     void PrintMenu();
     void LaunchLinkedListManagement();
     ~LinkedListManagement();
-
+  
   private:
     Node* root_node;
+    InsertNodeManager insert;
+    DeleteNodeManager remove;
+    RequestManager request; 
     LinkedListInfo list_info;
     NodePositionInfo node;
 };
+#endif /* LinkedListManagement_h */
