79fbf02dfd6169d8de630d04aade54afcdc501a4
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-17 23:48:52 -0800

    Update RequestManager.cpp Created a routine to get node data to insert at a certain node position
    Created a routine name InsertRequest to get node position and data and return it.

--- RequestManager.cpp
+++ RequestManager.cpp
@@ -16,3 +16,16 @@ int RequestManager::GetValidatedUserInput(int user_input) {
   ClearInputBuffer();
   return user_input;
 }
+
+struct NodePositionInfo RequestManager::InsertRequest(struct Node* current_node) {
+  std::cout << "\nADD NODE: \n---------\n";
+  std::cout << "Enter a number: ";
+  node.data = GetValidatedUserInput(node.data);
+  if (current_node != nullptr) {
+    int total_nodes = list_info.GetNumberOfNodes(current_node);
+    std::cout << "Enter a position between 1 and " << total_nodes + 1 << " to insert node: ";
+    node.position   = GetValidatedUserInput(node.position);
+  }
+  
+  return node;
+}
